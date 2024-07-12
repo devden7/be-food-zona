28aeaabba805605401a38a327678a0e4fab7b6c4
devden7 <devdenn7@gmail.com>
2024-07-12 08:54:59 +0700

    fix: adjustment validation in username form

--- src/user/user.validation.ts
+++ src/user/user.validation.ts
@@ -1,9 +1,14 @@
 import { z, ZodType } from 'zod';
 
 export class UserValidation {
-  static readonly REGISTER_USER: ZodType = z.object({
-    username: z.string().min(1).max(20),
-    name: z.string().min(1).max(50),
-    password: z.string().min(6).max(20),
-  });
+  static readonly REGISTER_USER: ZodType = z
+    .object({
+      username: z.string().min(1).max(20),
+      name: z.string().min(1).max(50),
+      password: z.string().min(6).max(20),
+    })
+    .refine((value) => !value.username.includes(' '), {
+      message: 'Please avoid using spaces in this input.',
+      path: ['username'],
+    });
 }
