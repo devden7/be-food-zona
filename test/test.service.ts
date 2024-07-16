27c7db4ce115b2373fe88651ed2461231ae1b3d7
devden7 <devdenn7@gmail.com>
2024-07-16 13:46:34 +0700

    fix: add method create and delete food in testService

--- test/test.service.ts
+++ test/test.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { PrismaServices } from '../src/common/prisma.service';
 import * as bcrypt from 'bcrypt';
 
@@ -19,6 +19,57 @@ export class TestService {
   async deleteDummyUser() {
     await this.prismaService.user.deleteMany({
       where: { username: 'test1' },
+    });
+  }
+
+  async createDummyFood() {
+    const insertDataCategories = await this.prismaService.category.upsert({
+      where: { name: 'Junk food' },
+      create: { name: 'Junk food' },
+      update: {},
+    });
+
+    await this.prismaService.food.create({
+      data: {
+        foodId: 15,
+        name: 'ayam geprek pedas',
+        description: 'ayam geprek pedas description dummy',
+        price: 15000,
+        category: {
+          create: [
+            {
+              categoryId: insertDataCategories.categoryId,
+            },
+          ],
+        },
+        restaurantName: 'Restaurant ayam geprek', // STILL HARD-CODED
+      },
+      include: { restaurant: true },
+    });
+  }
+
+  async deletedDummyFood() {
+    const findFood = await this.prismaService.food.findUnique({
+      where: {
+        foodId: 15,
+        restaurantName: 'Restaurant ayam geprek', // STILL HARD-CODED
+      },
+    });
+
+    if (!findFood) {
+      throw new HttpException('Food not found', 404);
+    }
+
+    await this.prismaService.foodCategory.deleteMany({
+      where: {
+        foodId: findFood.foodId,
+      },
+    });
+
+    await this.prismaService.food.deleteMany({
+      where: {
+        foodId: findFood.foodId,
+      },
     });
   }
 }
