85a0da2c94d6db0d385bd07f0f433ecd28c3f2ae
devden7 <devdenn7@gmail.com>
2024-07-20 14:21:20 +0700

    feat: testing API validation login user

--- src/user/user.validation.ts
+++ src/user/user.validation.ts
@@ -12,8 +12,13 @@ export class UserValidation {
       path: ['username'],
     });
 
-  static readonly LOGIN_USER: ZodType = z.object({
-    username: z.string().min(1).max(20),
-    password: z.string().min(6).max(20),
-  });
+  static readonly LOGIN_USER: ZodType = z
+    .object({
+      username: z.string().min(1).max(20),
+      password: z.string().min(6).max(20),
+    })
+    .refine((value) => !value.username.includes(' '), {
+      message: 'Please avoid using spaces in this input.',
+      path: ['username'],
+    });
 }
--- test/user.spec.ts
+++ test/user.spec.ts
@@ -130,4 +130,67 @@ describe('UserController Test', () => {
       expect(response.body.errors[0].path[0]).toBe('password');
     });
   });
+
+  describe('POST Login User : /api/login', () => {
+    it('Should be success login user', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/login')
+        .send({
+          username: 'test1',
+          password: '123456',
+        });
+
+      logger.info(response.body);
+      expect(response.status).toBe(201);
+      expect(response.body.data.username).toBe('test1');
+      expect(response.body.data.name).toBe('test1');
+      expect(response.body.data.restaurant).toBe('Restaurant ayam geprek');
+    });
+
+    it('Should be invalid login user (Username : Input with space)', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/login')
+        .send({
+          username: 'test 1',
+          password: '123456',
+        });
+
+      logger.info(response.body);
+      expect(response.status).toBe(400);
+      expect(response.body.errors[0].path[0]).toBe('username');
+      expect(response.body.errors[0].message).toBe(
+        'Please avoid using spaces in this input.',
+      );
+    });
+
+    it('Should be invalid when login  user (Username : Character less than 1, Password: Character less than 6)', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/login')
+        .send({
+          username: '',
+          password: '12345',
+        });
+
+      logger.info(response.body);
+      expect(response.status).toBe(400);
+      expect(response.body.errors[0].path[0]).toBe('username');
+      expect(response.body.errors[1].path[0]).toBe('password');
+    });
+
+    it('Should be invalid when login  user (Username & Password : Character more than 20)', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/login')
+        .send({
+          username:
+            'testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest',
+          password:
+            'testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest',
+        });
+
+      logger.info(response.body);
+      expect(response.status).toBe(400);
+      expect(response.body.errors[0].path[0]).toBe('username');
+      expect(response.body.errors[1].path[0]).toBe('password');
+    });
+  });
 });
