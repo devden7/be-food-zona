0d94bb80b68d6d2424827df5b35b08ab383ca895
devden7 <devdenn7@gmail.com>
2024-08-15 14:50:04 +0700

    fix: add new config cors

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,13 @@ import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
 
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://foodzona.vercel.app/'],
-    methods: ['POST', 'GET', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
